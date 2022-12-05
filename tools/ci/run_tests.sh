a0fbdacb228e2c97e7de4d6dc4aeeaa41b4e959a
sxnan <suxuannan95@gmail.com>
2022-12-05 17:00:02 +0800

    Add nightly build

---
+++ tools/ci/run_tests.sh
@@ -0,0 +1,28 @@
+#
+# Copyright 2022 The Feathub Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+set -e
+
+PROJECT_DIR=$(cd "$(dirname "$0")/../.."; pwd)
+
+python -m pip install apache-flink==1.15.2
+python -m pip install --upgrade feathub-nightly
+
+# Run the run_and_verify.sh script in each example folder
+for EXAMPLE_RUN_SCRIPT in "${PROJECT_DIR}"/*/run_and_verify.sh; do
+  echo "Running example ${EXAMPLE_RUN_SCRIPT}..."
+  bash "${EXAMPLE_RUN_SCRIPT}"
+  echo "Example ${EXAMPLE_RUN_SCRIPT} success."
+done
---
+++ tools/utils.sh
@@ -0,0 +1,42 @@
+#
+# Copyright 2022 The Feathub Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+function wait_for_port {
+  local PORT=$1
+	local COMPONENT_NAME=$2
+	local TIMEOUT_SECONDS=$((SECONDS + 120)) # timeout in 2 minutes
+  while ! nc -z 127.0.0.1 "${PORT}"; do
+    sleep 1
+    echo "Waiting for ${COMPONENT_NAME}..."
+    if [ "${SECONDS}" -ge "${TIMEOUT_SECONDS}" ]; then
+      echo "Timeout waiting for ${COMPONENT_NAME}."
+      exit 1
+    fi
+  done
+}
+
+function sort_and_compare_files {
+  local FILE1=$1
+	local FILE2=$2
+
+  echo "Sort and compare file ${FILE1} and ${FILE2}"
+	sort "${FILE1}" > "${FILE1}".sorted
+
+	# The return code of diff is non-zero if the files are different.
+	sort "${FILE2}" | diff - "${FILE1}".sorted
+
+	rm "${FILE1}".sorted
+}
